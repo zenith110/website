1d9a3072b8585541f508a056baa0293c3873c557
zenith110 <thequake@live.com>
2020-09-12 13:43:16 -0400

    Added another project

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def command_route():
     input = request.form["command-text"]
     
     if(input == "commands"):
-        return render_template("index.html", pstyleColor = "#F3EFE0", style = "display:none", result = "The following commands are available: project1, and clear.")
+        return render_template("index.html", pstyleColor = "#F3EFE0", style = "display:none", result = "The following commands are available: Gofetch, Pokemon, and clear.")
 
     elif(input == "clear"):
         return render_template("index.html", pstyleColor = "#F3EFE0", style = "display:none", result = "")
 
-    elif(input == "project1"):
+    elif(input == "Gofetch"):
         return render_template("index.html", 
         style = "flex", pstyleColor = "#F3EFE0",
         projectName = "Gofetch", 
@@ -26,9 +26,23 @@ def command_route():
         frameworks = "Frameworks and libraries used: DiscordGo, Flask, Selenium, BeautifulSoup4, Click, and Requests.",
         cloud = "Cloud and database used: Microsoft's Azure, and PostgreSQL",
         githubLink = "https://github.com/zenith110/Gofetch",
+        buttonstyle = "display:none;",
+        demoText = "Demo",
+        githubText = "Github Repo",
+        githubalign = "center")
+    elif(input == "Pokemon"):
+        return render_template("index.html", 
+        style = "flex", pstyleColor = "#F3EFE0",
+        projectName = "Pokemon Battle sim", 
+        goal = "Goal: Create a Peer to Peer battle simulator based off the popular Pokemon franchise", 
+        languages = "Languages used: Python",
+        frameworks = "Frameworks and libraries used: Pygame, PySide2",
+        cloud = "",
+        cloudbutton = "display:none;",
+        githubLink = "https://github.com/zenith110/pokemon_battle_sim",
         buttonstyle = "display:none;color: black",
         demoText = "Demo",
         githubText = "Github Repo",
         githubalign = "center")
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=80)
